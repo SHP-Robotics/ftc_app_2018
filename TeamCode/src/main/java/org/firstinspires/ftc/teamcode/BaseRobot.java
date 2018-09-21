5244b2c99d617f6cb1f6437e7aa3b96a55d43ca6
Matthew <31883267+mattkchun@users.noreply.github.com>
2018-09-21 15:04:39 -0700

    base for 10023 bot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BaseRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BaseRobot.java
@@ -6,13 +6,15 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
 public class BaseRobot extends OpMode {
-    public DcMotor leftMotor, rightMotor;
+    public DcMotor leftBackMotor, rightBackMotor, leftFrontMotor, rightFrontMotor;
     public ElapsedTime timer = new ElapsedTime();
 
     @Override
     public void init() {
-        leftMotor = hardwareMap.get(DcMotor.class, "leftMotor");
-        rightMotor = hardwareMap.get(DcMotor.class, "rightMotor");
+        leftBackMotor = hardwareMap.get(DcMotor.class, "leftBackMotor");
+        rightBackMotor = hardwareMap.get(DcMotor.class, "rightBackMotor");
+        leftFrontMotor = hardwareMap.get(DcMotor.class, "leftFrontMotor");
+        rightFrontMotor = hardwareMap.get(DcMotor.class, "rightFrontMotor");
     }
 
     @Override
@@ -23,20 +25,22 @@ public class BaseRobot extends OpMode {
 
     @Override
     public void loop() {
-        telemetry.addData("D00 Left Motor Enc: ", get_left_motor_enc());
-        telemetry.addData("D01 Right Motor Enc: ", get_right_motor_enc());
-        telemetry.addData("D02 Left Joystick Y: ", gamepad1.left_stick_y);
-        telemetry.addData("D03 Left Joystick X: ", gamepad1.left_stick_x);
-        telemetry.addData("D04 Right Joystick Y: ", gamepad1.right_stick_y);
-        telemetry.addData("D05 Right Joystick X: ", gamepad1.right_stick_x);
+        telemetry.addData("D00 Left Back Motor Enc: ", get_left_back_motor_enc());
+        telemetry.addData("D01 Right Back Motor Enc: ", get_right_back_motor_enc());
+        telemetry.addData("D02 Left Front Motor Enc: ", get_left_front_motor_enc());
+        telemetry.addData("D03 Right Front Motor Enc: ", get_right_front_motor_enc());
+//        telemetry.addData("D04 Left Joystick Y: ", gamepad1.left_stick_y);
+//        telemetry.addData("D05 Left Joystick X: ", gamepad1.left_stick_x);
+//        telemetry.addData("D06 Right Joystick Y: ", gamepad1.right_stick_y);
+//        telemetry.addData("D07 Right Joystick X: ", gamepad1.right_stick_x);
     }
 
     public boolean auto_drive(double power, double inches) {
-        double TARGET_ENC = ConstantVariables.K_PPIN_DRIVE * inches * Math.sqrt(2);
-
+        double TARGET_ENC = ConstantVariables.K_PPIN_DRIVE * inches;
+        telemetry.addData("Target_enc: ", TARGET_ENC);
         double left_speed = -power;
         double right_speed = power;
-        double error = get_left_motor_enc() - get_right_motor_enc();
+        double error = -get_left_front_motor_enc() - get_right_front_motor_enc();
 
         error /= ConstantVariables.K_DRIVE_ERROR_P;
         left_speed -= error;
@@ -44,13 +48,16 @@ public class BaseRobot extends OpMode {
 
         left_speed = Range.clip(left_speed, -1, 1);
         right_speed = Range.clip(right_speed, -1, 1);
-        leftMotor.setPower(left_speed);
-        rightMotor.setPower(right_speed);
+        leftBackMotor.setPower(left_speed);
+        rightBackMotor.setPower(right_speed);
+        leftFrontMotor.setPower(left_speed);
+        rightFrontMotor.setPower(right_speed);
 
-        if (Math.abs(get_left_motor_enc()) >= TARGET_ENC &&
-                Math.abs(get_right_motor_enc()) >= TARGET_ENC) {
-            leftMotor.setPower(0);
-            rightMotor.setPower(0);
+        if (Math.abs(get_left_front_motor_enc()) >= TARGET_ENC && Math.abs(get_right_front_motor_enc()) >= TARGET_ENC) {
+            leftBackMotor.setPower(0);
+            rightBackMotor.setPower(0);
+            leftFrontMotor.setPower(0);
+            rightFrontMotor.setPower(0);
             return true;
         }
         return false;
@@ -65,14 +72,17 @@ public class BaseRobot extends OpMode {
         double TARGET_ENC = Math.abs(ConstantVariables.K_PPDEG_DRIVE * degrees);
         telemetry.addData("D99 TURNING TO ENC: ", TARGET_ENC);
 
-        if (Math.abs(get_left_motor_enc()) >= TARGET_ENC &&
-                Math.abs(get_right_motor_enc()) >= TARGET_ENC) {
-            leftMotor.setPower(0);
-            rightMotor.setPower(0);
+        if (Math.abs(get_left_front_motor_enc()) >= TARGET_ENC && Math.abs(get_right_front_motor_enc()) >= TARGET_ENC) {
+            leftBackMotor.setPower(0);
+            rightBackMotor.setPower(0);
+            leftFrontMotor.setPower(0);
+            rightFrontMotor.setPower(0);
             return true;
         } else {
-            leftMotor.setPower(power);
-            rightMotor.setPower(power);
+            leftBackMotor.setPower(power);
+            rightBackMotor.setPower(power);
+            leftFrontMotor.setPower(power);
+            rightFrontMotor.setPower(power);
         }
         return false;
     }
@@ -81,29 +91,49 @@ public class BaseRobot extends OpMode {
         double leftPower = Range.clip(leftPwr, -1.0, 1.0);
         double rightPower = Range.clip(rightPwr, -1.0, 1.0);
 
-        leftMotor.setPower(leftPower);
-        rightMotor.setPower(rightPower);
+        leftBackMotor.setPower(leftPower);
+        rightBackMotor.setPower(rightPower);
+        leftFrontMotor.setPower(leftPower);
+        rightFrontMotor.setPower(rightPower);
     }
 
     public void reset_drive_encoders() {
-        leftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        rightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftBackMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightBackMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftFrontMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightFrontMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-        leftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        rightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftBackMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightBackMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftFrontMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightFrontMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
 
-    public int get_left_motor_enc() {
-        if (leftMotor.getMode() != DcMotor.RunMode.RUN_USING_ENCODER) {
-            leftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    public int get_left_back_motor_enc() {
+        if (leftBackMotor.getMode() != DcMotor.RunMode.RUN_USING_ENCODER) {
+            leftBackMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         }
-        return leftMotor.getCurrentPosition();
+        return leftBackMotor.getCurrentPosition();
     }
 
-    public int get_right_motor_enc() {
-        if (rightMotor.getMode() != DcMotor.RunMode.RUN_USING_ENCODER) {
-            rightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    public int get_right_back_motor_enc() {
+        if (rightBackMotor.getMode() != DcMotor.RunMode.RUN_USING_ENCODER) {
+            rightBackMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         }
-        return rightMotor.getCurrentPosition();
+        return rightBackMotor.getCurrentPosition();
+    }
+
+    public int get_left_front_motor_enc() {
+        if (leftBackMotor.getMode() != DcMotor.RunMode.RUN_USING_ENCODER) {
+            leftBackMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        }
+        return leftBackMotor.getCurrentPosition();
+    }
+
+    public int get_right_front_motor_enc() {
+        if (rightFrontMotor.getMode() != DcMotor.RunMode.RUN_USING_ENCODER) {
+            rightFrontMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        }
+        return rightFrontMotor.getCurrentPosition();
     }
 }
